fa50c5581ed878dcbf59e7bf6976921f9a8157ff
sai charan <34817986+saicharan1997@users.noreply.github.com>
2017-12-28 21:12:19 +0530

    Update TwitterAppBar.js

--- src/Homepage/TwitterAppBar.js
+++ src/Homepage/TwitterAppBar.js
@@ -15,11 +15,14 @@ const styles={
         backgroundColor: "#1da1f2",
         color:"#ffffff",
         padding: 0,
-        minHeight: "38px",
+        minHeight: "33px",
+        minWidth:"73px",
         border: "1px solid",
-        borderRadius: "20px",
+        borderRadius: 50,
         textTransform:"none",
-        fontSize:"13px",        
+        fontSize:"14px",
+        marginTop:4,
+        fontWeight:600,    
     },
 };
 const primary = '#1DA1F2';
@@ -60,36 +63,39 @@ class TwitterAppBar extends Component{
         const classes = this.props.classes;
         return(
             <div>
-                <AppBar position="fixed" style={{backgroundColor:"#ffffff"}}>
+                <AppBar position="static" style={{backgroundColor:"#ffffff"}}>
                     <Grid container direction="row" spacing={0}>
-                        <Grid item xs={0}>
+                        <Grid item xs={1} style={{marginLeft:"60px"}}>
                         </Grid>
                         <Grid item>
                             <Tabs value={this.state.value} style={{color:primary}} indicatorColor={primary} fullWidth={false} onChange={this.handleChange}>
-                                <Tab label="Home" style={{minWidth:0,textDecoration:"none",color:"#000000"}} href="/"/>
-                                <Tab label="Moments" style={{minWidth:0,color:"#000000"}}/>
-                                <Tab label="Notifications" style={{minWidth:0,color:"#000000"}}/>
-                                <Tab label="Messages" style={{minWidth:0,color:"#000000"}}/>
+                                <Tab label="Home" style={{minWidth:0,textDecoration:"none",fontFamily:'Segoe UI'}} href="/"/>
+                                <Tab label="Moments" style={{minWidth:0,textDecoration:"none",fontFamily:'Segoe UI'}}/>
+                                <Tab label="Notifications" style={{minWidth:0,textDecoration:"none",fontFamily:'Segoe UI'}}/>
+                                <Tab label="Messages" style={{minWidth:0,textDecoration:"none",fontFamily:'Segoe UI'}}/>
                             </Tabs>
                         </Grid>
-                        <Grid item xs={4} style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
-                            <i className="fa fa-twitter" style={{color:"#1da1f2",fontSize:"22px"}}>
+                        <Grid item xs={1} style={{display:"flex",justifyContent:"center",alignItems:"center",marginLeft:"50px"}}>
+                            <i className="fa fa-twitter" style={{color:"#1da1f2",fontSize:"22px",cursor: "pointer"}}>
                             </i>
                         </Grid>
-                        <Grid item xs={2} className="searchMe" style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
-                            <input type="text" value={this.state.searchString} className="navSearch" placeholder="Search Twitter" spellCheck="false" autoComplete="false" contentEditable="false" onKeyPress={this.searchIt} onChange={evt=>this.updateInputVal(evt)}
+                        <Grid item xs={2} className="searchMe" style={{display:"flex",justifyContent:"center",alignItems:"center",marginLeft:"150px"}}>
+                            <input type="text" value={this.props.searchString} className="navSearch" placeholder="Search Twitter" spellCheck="false" autoComplete="false" onKeyPress={this.searchIt} onChange={evt=>this.updateInputVal(evt)}
                             />
-                            <span className="fa fa-search" aria-hidden="true">
+                            <span className="fa fa-search" aria-hidden="true" style={{paddingRight:"20px"}}>
                             </span>
-                        </Grid>
-                        <Grid item  xs={1} style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
-                            <Avatar src={mydp} size={50}/>
+                         
                         </Grid>
                         <Grid item style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
-                            <Button  color="primary" className={classes.tweetButton}>
+                        <Avatar src={mydp} size={25} style={{width: 32,height: 32,marginTop:4,cursor: "pointer"}}/>
+                        </Grid>
+                        
+                        <Grid item style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+                            <Button  color="primary" className={classes.tweetButton} style={{marginLeft:"10px"}}>
                                 Tweet
                             </Button>
-                        </Grid>
+                        </Grid>    
+                        
                     </Grid>
                 </AppBar>
             </div>
